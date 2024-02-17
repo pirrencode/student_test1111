3870ec9ab1856d424214b9814705881037f7d832
teacher <teacher@riceba.lv>
2024-02-17 15:37:03 +0200

    Klases darbs ar studejosiem.

---
+++ uzdevums2/my_script2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ls
+mkdir uzdevums2
+cd uzdevums2
+ls
+touch my_script2.sh
+
+
---
+++ uzdevums2/uzdevums2/my_script3.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ls
+cd Desktop
+ls
+git clone https://github.com/pirrencode/student_test1111.git
+ls
+cd student_test1111/
+git checkout develop
+touch my_bash_script.sh
+ls
+nano my_bash_script.sh
+nano my_bash_script.sh
+nano my_bash_script.sh
+chmod a+x my_bash_script.sh
+ls
+./my_bash_script.sh
+git status
+git add my_bash_script.sh
+git commit -m "RISEBA: My first bash script."
+git push
+nano my_bash_script.sh
+./my_bash_script.sh
+pwd
+ mkdir uzdevums2
+ls
+cd uzdevums2/
+ls
+touch my_script2.sh
+ls
+nano my_script2.sh
+nano my_script2.sh
+ls
+./my_script2.sh
+ls
+cd uzdevums2/
+ls
+history
+
+
